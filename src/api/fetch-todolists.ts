e2c16a80fb8ff94a584be5d63a1373de1db8ce75
Oleg655 <savinoleg95@gmail.com>
2022-10-26 20:00:55 +0300

    create .env

--- src/api/fetch-todolists.ts
+++ src/api/fetch-todolists.ts
@@ -1,13 +1,11 @@
 const fetchTodoList = {
-  getTodoList: async () => {
-    const response = await fetch(
-      'https://social-network.samuraijs.com/api/1.1/todo-lists',
-      {
-        method: 'GET',
-        credentials: 'include',
-      },
-    );
+  getTodoList: async (baseUrl: string | undefined, endpoint: string | undefined) => {
+    const response = await fetch(`${baseUrl}${endpoint}`, {
+      method: 'GET',
+      credentials: 'include',
+    });
     const data = await response.json();
+    console.log(data);
     return data;
   },
 };
