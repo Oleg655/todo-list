54962506c5d6089e613735c01a2933fb29857ca8
Oleg655 <savinoleg95@gmail.com>
2022-10-27 19:34:16 +0300

    testing api for post

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import fetchTodoList from 'api/fetch-todolists';
 import './App.css';
 
-const url = process.env.REACT_APP_BASE_URL;
-
 const App: React.FC = () => {
-  const data = fetchTodoList.getTodoList(url);
-  const postData = fetchTodoList.postTodoList(url);
-  console.log(data);
-  console.log(postData);
-  console.log(data);
   return (
     <div className="App">
-      <header className="App-header">{JSON.stringify(data)}</header>
+      <header className="App-header">app</header>
     </div>
   );
 };
