e9cf3ffd990db590fa9e5bcaeddecd9805482b72
Oleg655 <savinoleg95@gmail.com>
2022-10-28 12:54:46 +0300

    dlete and update methods for todolist

--- src/api/fetch-todolists.ts
+++ src/api/fetch-todolists.ts
@@ -9,11 +9,39 @@ const fetchTodoList = {
     const data = await response.json();
     return data;
   },
-  postTodoList: async (baseUrl: string | undefined) => {
-    const response = await fetch(`${baseUrl}/todo-lists`, {
+  postTodoList: async (baseUrl: string | undefined, newTitle: { title: string }) => {
+    const response = await fetch(`${baseUrl}/todo-lists/`, {
       method: 'POST',
       credentials: 'include',
-      body: JSON.stringify({ title: 'newTitle' }),
+      body: JSON.stringify(newTitle),
+      headers: {
+        'API-KEY': apiKey,
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await response.json();
+    return data;
+  },
+  deleteTodoList: async (baseUrl: string | undefined, todoListId: string) => {
+    const response = await fetch(`${baseUrl}/todo-lists/${todoListId}`, {
+      method: 'DELETE',
+      credentials: 'include',
+      headers: {
+        'API-KEY': apiKey,
+      },
+    });
+    const data = await response.json();
+    return data;
+  },
+  updateTodoList: async (
+    baseUrl: string | undefined,
+    todoListId: string,
+    newTitle: { title: string },
+  ) => {
+    const response = await fetch(`${baseUrl}/todo-lists/${todoListId}`, {
+      method: 'PUT',
+      credentials: 'include',
+      body: JSON.stringify(newTitle),
       headers: {
         'API-KEY': apiKey,
         'Content-Type': 'application/json',
